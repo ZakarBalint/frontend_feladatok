924f91d10ed733fcd8ee02e9cf40497f8bf35092
ZakarBalint <szt2zakbal@vasvari.org>
2023-10-13 08:42:31 +0200

    a

---
+++ objekt1/Object1.js
@@ -0,0 +1,15 @@
+window.onload = function()
+{
+
+    const user = {};
+
+    user.firstname = "Jhon";
+    user.lastname = "Smith";
+
+    console.log(user);
+
+    user.firstname = "Paul";
+
+    console.log(user);
+
+}
